573686d17f1f189383b745632a2e29be2d839a70
sanje <sanje@DESKTOP-EL8L30L>
2022-10-05 19:12:14 -0400

    Add project files.

---
+++ Lab_01/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Lab_01.Models;
+
+namespace Lab_01.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Team> Teams { get; set; }
+
+    }
+}
